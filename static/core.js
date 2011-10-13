5279035870ab06f90932a3c0c0afac270a8d8db7
Jacob Haslehurst <jacob@haslehurst.net>
2011-10-14 03:04:52 +0800

    working on getting the form to generate the correct json, dealing with #2

--- static/core.js
+++ static/core.js
@@ -13,24 +13,31 @@
 			}
 
 			// resistances
-			function add_resistance() {
-				html = $('.resistance').filter(':first').html();
-				div = "<div class='resistance'>" + html + "</div>";
-				$('.resistance').filter(':last').after(div);
-			}
-
-			// skills
+			var skill_count = 0;
 			function add_skill() {
 				html = $('.skill').filter(':first').html();
 				div = "<div class='skill'>" + html + "</div>";
 				$('.skill').filter(':last').after(div);
+
+				//set the id correctly
+				skill_count ++;
+				$('.skill_input').filter(':last').attr('name', 'skill[' + skill_count + ']');
 			}
 
 			// natural attacks
+			var natural_attack_count = 0;
 			function add_natural_attack() {
 				html = $('.natural_attack').filter(':first').html();
 				div = "<div class='natural_attack'>" + html + "</div>";
 				$('.natural_attack').filter(':last').after(div);
+
+				//lets get this id set
+				natural_attack_count ++;
+				$('natural_attack').filter(':last').children(':input').each(function() {
+					console.log('hi');
+					chunks = $(this).attr('name').split('.');
+					$(this).attr('name', 'natural_attack['+natural_attack_count+'].hithere.'+chunks[1]);
+				});
 			}
 
 
@@ -39,20 +46,27 @@ $(document).ready(function() {
 
 	// new monser page
 		// form submit function
-		$('#new_monster_form').submit(function(){
-			console.log($(this).serializeArray());
+		//$('#new_monster_form').submit(function(){
+		$('#submit_form').click(function(){
+			//var natural_attacks = new Array();
+			//$('.natural_attack').each(function (i) {
+			//
+			//	$('input', this).each(function(){
+			//		key = $(this).attr('name');
+			//		value = $(this).bal();
+			//		natural_attacks[i][key] = value;
+			//	})
+			//})
+			//console.log(natural_attacks);
+			//alert(natural_attacks);
+			//return false;
+			var formData = $('#new_monster_form').toObject();
+			console.log(formData);
 			return false;
 		})
 
 		// bind form functions
 			// bind resistance
-			$('#resistance_add').click(function(){
-				add_resistance();
-				bind_delete_x();
-				return false;
-			})
-
-			// bind skill
 			$('#skill_add').click(function(){
 				add_skill();
 				bind_delete_x();
